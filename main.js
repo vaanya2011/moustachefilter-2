637741a097bb06d6da9f0dafd9c9559c03c0dca1
vaanya2011 <100355303+vaanya2011@users.noreply.github.com>
2023-05-30 17:28:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+nose_x="";
+nose_y="";
+
+function preload()
+{
+
+
+}
+
+
+function setup()
+{
+
+    canvas=createCanvas(300, 300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+image(video, 0, 0, 300, 300);
+}
+
+function take_snapshot()
+{
+
+    save("myfilter.png");
+}
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        nose_x=results[0].pose.nose.x-40;
+        nose_y=results[0].pose.nose.y;
+    }
+}
